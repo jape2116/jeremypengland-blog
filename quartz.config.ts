cc1179b90e326f28b5410d930be3b53e11c50882
jape2116 <jape2116@gmail.com>
2025-08-21 13:54:00 -0500

    Quartz sync: Aug 21, 2025, 1:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "QJeremy P. England",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
