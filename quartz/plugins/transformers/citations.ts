6e7bcf7f92adff01539a75c77b2c4a3504ea38d6
jape2116 <jape2116@gmail.com>
2025-08-26 15:08:20 -0500

    Quartz sync: Aug 26, 2025, 3:08 PM

--- quartz/plugins/transformers/citations.ts
+++ quartz/plugins/transformers/citations.ts
@@ -14,7 +14,7 @@ const defaultOptions: Options = {
   bibliographyFile: "/2025-08-25-ZoteroLibraryExportSync.bib",
   suppressBibliography: false,
   linkCitations: false,
-  csl: https://github.com/citation-style-language/styles/blob/master/chicago-notes-bibliography-subsequent-author-title-17th-edition.csl,
+  csl: "https://www.zotero.org/styles?q=id%3Achicago-notes-bibliography-subsequent-author-title-17th-edition",
 }
 
 export const Citations: QuartzTransformerPlugin<Partial<Options>> = (userOpts) => {
